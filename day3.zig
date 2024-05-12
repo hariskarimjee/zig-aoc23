9c5d5a00cd35f5e7569bc09ea03930c8407519d9
Haris Karimjee <haris.karimjee@gmail.com>
2024-05-12 19:02:20 +0100

    d3p1 solve - wrong approach for p2

--- day3.zig
+++ day3.zig
@@ -1,48 +1,133 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const testInput =
-    \\467..114..
-    \\...*......
-    \\..35..633.
-    \\......#...
-    \\617*......
-    \\.....+.58.
-    \\..592.....
-    \\......755.
-    \\...$.*....
-    \\.664.598..
-;
 
-const Point = struct { x: usize, y: usize };
+const Number = struct { x: usize, y: usize, xspan: usize, num: u64, symbol: u8 };
 
-pub fn main() void {}
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const result = try solve(allocator, "data/day3.txt");
+    std.debug.print("answer: {d}\n", .{result});
+}
 
-fn findSymbols(allocator: Allocator, schematic: []const u8) !std.ArrayList(Point) {
-    var lines = std.mem.splitScalar(u8, schematic, '\n');
-    var symbols = std.ArrayList(Point).init(allocator);
-    var i: u32 = 0;
-    while (lines.next()) |row| : (i += 1) {
-        for (row, 0..) |char, col| {
-            if (!std.ascii.isDigit(char) and char != '.') {
-                try symbols.append(Point{ .x = i, .y = col });
+fn solve(allocator: Allocator, filepath: []const u8) !u64 {
+    var sum: u64 = 0;
+    const inputData = try std.fs.cwd().readFileAlloc(allocator, filepath, 1e6);
+    defer allocator.free(inputData);
+    const inputMatrix = try getMatrix(allocator, inputData);
+    defer inputMatrix.deinit();
+    const numbers = try findNumbers(allocator, inputMatrix);
+    defer numbers.deinit();
+    for (numbers.items) |*number| {
+        if (try isTouchingSymbol(number, inputMatrix)) {
+            sum += number.num;
+        }
+    }
+    return sum;
+}
+
+fn getMatrix(allocator: Allocator, inputData: []const u8) !std.ArrayList([]const u8) {
+    var inputLines = std.mem.splitScalar(u8, inputData, '\n');
+    var inputMatrix = std.ArrayList([]const u8).init(allocator);
+    while (inputLines.next()) |line| {
+        if (line.len > 0)
+            try inputMatrix.append(line);
+    }
+    return inputMatrix;
+}
+
+fn findNumbers(allocator: Allocator, inputMatrix: std.ArrayList([]const u8)) !std.ArrayList(Number) {
+    var numbers = std.ArrayList(Number).init(allocator);
+    var num: Number = undefined;
+    var midNumber: bool = false;
+    const input = inputMatrix.items;
+    for (input, 0..) |line, i| {
+        for (line, 0..) |char, j| {
+            if (std.ascii.isDigit(char)) {
+                if (!midNumber) {
+                    midNumber = true;
+                    num.x = j;
+                    num.y = i;
+                    num.xspan = 1;
+                } else {
+                    num.xspan += 1;
+                    if (j == line.len - 1) {
+                        num.num = try std.fmt.parseInt(u64, line[num.x..(num.x + num.xspan)], 0);
+                        try numbers.append(num);
+                        midNumber = false;
+                    }
+                }
+            } else {
+                if (midNumber) {
+                    num.num = std.fmt.parseInt(u64, line[num.x..(num.x + num.xspan)], 0) catch |e| switch (e) {
+                        std.fmt.ParseIntError.InvalidCharacter => {
+                            std.debug.print("Couldn't parse integer: {s}, xpos: {d}, ypos: {d}.\n Line: {s}", .{ line[num.x..(num.x + num.xspan)], num.x, num.y, line });
+                            return e;
+                        },
+                        std.fmt.ParseIntError.Overflow => {
+                            std.debug.print("Integer overflow on type.", .{});
+                            return e;
+                        },
+                    };
+
+                    try numbers.append(num);
+                    midNumber = false;
+                }
             }
         }
     }
-    return symbols;
+    return numbers;
 }
 
-test "find symbols basic" {
-    const symlist = try findSymbols(std.testing.allocator, "...*....\n....*...");
-    defer symlist.deinit();
-    try std.testing.expect(symlist.items[0].x == 0);
-    try std.testing.expect(symlist.items[0].y == 3);
-    try std.testing.expect(symlist.items[1].x == 1);
-    try std.testing.expect(symlist.items[1].y == 4);
+fn isTouchingSymbol(number: *Number, inputMatrix: std.ArrayList([]const u8)) !bool {
+    const miny: usize = if (@as(isize, @intCast(number.y)) - 1 < 0) 0 else number.y - 1;
+    const maxy: usize = if (number.y + 2 >= inputMatrix.items.len) (inputMatrix.items.len) else number.y + 2;
+    for (inputMatrix.items[miny..maxy]) |line| {
+        const minx: usize = if (@as(isize, @intCast(number.x)) - 1 < 0) 0 else number.x - 1;
+        const maxx: usize = if (number.x + number.xspan + 1 >= line.len) (line.len) else number.x + number.xspan + 1;
+        for (line[minx..maxx]) |char| {
+            if (!std.ascii.isDigit(char) and (char != '.')) {
+                number.symbol = char;
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-test "find symbols test input" {
-    const symlist = try findSymbols(std.testing.allocator, testInput);
-    defer symlist.deinit();
-    try std.testing.expect(symlist.items[0].x == 1 and symlist.items[0].y == 3);
-    try std.testing.expect(symlist.items[1].x == 3 and symlist.items[1].y == 6);
+test "get input matrix" {
+    const inputData = try std.fs.cwd().readFileAlloc(std.testing.allocator, "data/day3test.txt", 1e3);
+    defer std.testing.allocator.free(inputData);
+    const inputMatrix = try getMatrix(std.testing.allocator, inputData);
+    defer inputMatrix.deinit();
+    try std.testing.expect(inputMatrix.items[1][3] == '*');
 }
+
+test "get first nums" {
+    const inputData = try std.fs.cwd().readFileAlloc(std.testing.allocator, "data/day3test.txt", 1e3);
+    defer std.testing.allocator.free(inputData);
+    const inputMatrix = try getMatrix(std.testing.allocator, inputData);
+    defer inputMatrix.deinit();
+    const numbers = try findNumbers(std.testing.allocator, inputMatrix);
+    defer numbers.deinit();
+    try std.testing.expect(numbers.items[0].num == 467);
+    try std.testing.expect(numbers.items[0].xspan == 3);
+}
+
+test "get num symbol" {
+    const inputData = try std.fs.cwd().readFileAlloc(std.testing.allocator, "data/day3test.txt", 1e3);
+    defer std.testing.allocator.free(inputData);
+    const inputMatrix = try getMatrix(std.testing.allocator, inputData);
+    defer inputMatrix.deinit();
+    var numbers = try findNumbers(std.testing.allocator, inputMatrix);
+    defer numbers.deinit();
+    try std.testing.expect(try isTouchingSymbol(&numbers.items[0], inputMatrix));
+    try std.testing.expect(numbers.items[0].symbol == '*');
+}
+
+test "solve" {
+    const result = try solve(std.testing.allocator, "data/day3test.txt");
+    try std.testing.expect(result == 4361);
+}
+
+//answer 532428
